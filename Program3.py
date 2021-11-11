814a71ff458b3f63357d085c6883cf6cec41c260
Magno <clarenceamagno@iskolarngbayan.pup.edu.ph>
2021-11-11 14:42:19 +0800

    Fourth commit

--- Program3.py
+++ Program3.py
@@ -1,34 +1,33 @@
 # Create a program which you will enter the amount of money you have, it will also ask for the price of an apple.
 # Display the maximum number of apples that you can buy and the remaining money that you will have.
 
-def getInput(dataIn):
+def getInput():
     """takes an input and returns a value depending on the parameter requested
     
         arg: dataIn - a string that determines the output value
         return: a value from the userInput
         
     """
+    # ask the user for how much money they have and how much an apple is.
     while 1:
-        if dataIn == "money":
-            bill = int(input("How much money do you have?: "))
-        else:
-            bill = int(input("How much is an apple?: "))
-        if bill < 0:
+        money = int(input("How much money do you have?: "))
+        appleCost = int(input("How much is an apple?: "))
+        
+        if money < 0 and appleCost < 0:
             print("You cannot input negative number.")
         else:
             break
-    return bill
+    return money, appleCost
 
 print("program 3")
-money = getInput("money")
-appleCost = getInput("appleCost")
 
+bill = getInput()
+
+# Show the user how many apples they can buy with their money and the change they will get if they have one.
 while 1:
-    if money >= appleCost:
-        print(f"you can buy {int(money / appleCost)} apple(s) and your change is ₱{money%appleCost}")
+    if bill[0] >= bill[1]:
+        print(f"you can buy {int(bill[0] / bill[1])} apple(s) and your change is ₱{bill[0]%bill[1]}")
         break
     else:
-        print(f"you cannot afford an apple. You are missing P{appleCost - money}")
-        money = getInput("money")
-    
-exit()
+        print(f"you cannot afford an apple. You are missing P{bill[1] - bill[0]}")
+        bill = getInput()
